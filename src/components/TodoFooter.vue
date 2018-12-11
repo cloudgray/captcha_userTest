546ddc639ec862ecc521db42a41c7524af18ad08
최규현 <cloudgray@naver.com>
2018-12-11 22:30:52 +0900

    ..

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,42 @@
+<template lang="html">
+  <div class="clearAllContainer">
+    <span class="accuracy">Accuracy: {{accuracy}} %</span>
+    <br />
+    <br />
+    <span class="clearAllBtn" @click="clearTodo">Clear All</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      accuracy: 100,
+    }
+  },
+  methods: {
+    clearTodo() {
+      this.$emit('removeAll');
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.ClearAllContainer {
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  background-color: white;
+  border-radius: 5px;
+  margin: 0 auto;
+}
+
+.clearAllBtn {
+  color: #e20303;
+  display: black;
+}
+.accuracy {
+  font-weight: bold;
+}
+</style>
---
+++ src/components/TodoImage.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <span class="removeBtn" type="button">
+    <i class="fas fa-arrow-left" aria-hidden="true"></i>
+  </span>
+  <span class="level">Level-3</span>
+  <span class="removeBtn" type="button">
+    <i class="fas fa-arrow-right" aria-hidden="true"></i>
+  </span>
+  <br />
+  <img src='../assets/7688_lv2_16043.png' width="200">
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="css">
+  .level {
+    font-size: 20px;
+  }
+</style>
